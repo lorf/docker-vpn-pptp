2d9152db32633f33ecfae7d09d9216fb2c62ab9f
Dmitry Frolov <dmitry.frolov@gmail.com>
2019-03-26 09:58:39 +0700

    Allow to override network settings

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,12 @@ COPY ./etc/ppp/pptpd-options /etc/ppp/pptpd-options
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod 0700 /entrypoint.sh
 
+# Default network settings
+ENV NETWORK=10.99.99.0/24
+ENV LOCALIP=10.99.99.1
+ENV IPRANGE=10.99.99.100-200
+ENV DNS1=8.8.8.8
+ENV DNS2=1.1.1.1
+
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["pptpd", "--fg"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,19 @@ addrule() {
         iptables -t "$table" -A "$@"
 }
 
+sed -i "s!%%LOCALIP%%!$LOCALIP!g; s!%%IPRANGE%%!$IPRANGE!g" \
+    /etc/pptpd.conf
+
+sed -i "s!%%DNS1%%!$DNS1!g" /etc/ppp/pptpd-options
+if [ "$DNS2" ]; then
+    sed -i "s!%%DNS2%%!$DNS2!g" /etc/ppp/pptpd-options
+else
+    sed -i '/%%DNS2%%/d' /etc/ppp/pptpd-options
+fi
+
 # configure firewall
-addrule nat POSTROUTING -s 10.99.99.0/24 ! -d 10.99.99.0/24 -j MASQUERADE
-addrule filter FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+addrule nat POSTROUTING -s "$NETWORK" ! -d "$NETWORK" -j MASQUERADE
+addrule filter FORWARD -s "$NETWORK" -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 addrule filter INPUT -i ppp+ -j ACCEPT
 addrule filter OUTPUT -o ppp+ -j ACCEPT
 addrule filter FORWARD -i ppp+ -j ACCEPT
